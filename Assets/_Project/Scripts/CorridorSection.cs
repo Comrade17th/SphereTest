fb888176289d96ebf157d1fd5281678e87e9ad73
nasusfen <ravshannedonat@gmail.com>
2024-12-06 14:22:48 +0300

    Random corridor section generation added, random size, random template

--- Assets/_Project/Scripts/CorridorSection.cs
+++ Assets/_Project/Scripts/CorridorSection.cs
@@ -1,9 +1,37 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace _Project.Scripts
 {
     public class CorridorSection : MonoBehaviour
     {
-        
+        [SerializeField] private Transform _leftWall;
+        [SerializeField] private Transform _rightWall;
+
+        [SerializeField] private Transform _minBorderLeft;
+        [SerializeField] private Transform _minBorderRight;
+
+        [SerializeField] private List<Transform> _spawnPoints;
+
+        [SerializeField] private bool _isRandomWidth;
+
+        public void Init()
+        {
+            if (_isRandomWidth)
+            {
+                _leftWall.position = RandomGenerator.GetPositionX(_leftWall,_leftWall, _minBorderLeft);
+                _rightWall.position = RandomGenerator.GetPositionX(_rightWall, _minBorderRight, _rightWall);
+            }
+        }
+    }
+
+    public static class RandomGenerator
+    {
+        public static Vector3 GetPositionX(Transform original, Transform minInclusive, Transform maxInclusive)
+        {
+            float randomX = Random.Range(minInclusive.position.x, maxInclusive.position.x);
+            return new Vector3(randomX, original.position.y, original.position.z);
+        }
     }
 }
--- Assets/_Project/Scripts/CorridorSectionSpawner.cs
+++ Assets/_Project/Scripts/CorridorSectionSpawner.cs
@@ -1,16 +1,19 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace _Project.Scripts
 {
 	public class CorridorSectionSpawner : MonoBehaviour
 	{
-		[SerializeField] private GameObject _sectionPrefab; // to asset path
-		// add assets service
+		[SerializeField] private List<GameObject> _sectionsPrefabs;
 		
 		public CorridorSection Spawn(Vector3 at)
 		{
-			GameObject section = Instantiate(_sectionPrefab, at, Quaternion.identity);
-			return section.GetComponent<CorridorSection>();
+			GameObject sectionPrefab = _sectionsPrefabs[Random.Range(0, _sectionsPrefabs.Count - 1)];
+			GameObject section = Instantiate(sectionPrefab, at, Quaternion.identity);
+			var corridorSection = section.GetComponent<CorridorSection>();
+			corridorSection.Init();
+			return corridorSection;
 		}
 	}
 }
